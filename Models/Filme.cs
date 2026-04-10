17ad894279e858e8902a42030d0d78c0fdab2ce8
025.030174 <025.030174@C305-3023683>
2026-04-10 11:28:02 -0300

    adicionando arquivos

---
+++ Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O gênero do filme deve conter no máximo 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70,600, ErrorMessage ="A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+    public int Id { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(opts =>
+opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
